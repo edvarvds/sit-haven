834f996ea4cf2fbf77e5f9a5f2def8e7025f8d53
edvar <test@gmail.com>
2025-03-13 06:17:37 -0300

    first commit

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def serve_static(filename):
     return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
